a1d8928479f16be061939c141759d6844e3ae424
Daniel Mikolai <d.mikolai@gmx.de>
2023-09-16 22:04:51 +0200

    final out folder

--- kungfu-komplett/next.config.js
+++ kungfu-komplett/next.config.js
@@ -4,11 +4,15 @@ module.exports = {
     experimental: {
         appDir: true
     },
-    i18n: {
-        // providing the locales supported by your application
-        // locales: ['en-US', 'de-DE'],
-        locales: ['de-DE'],
-        //  default locale used when the non-locale paths are visited
-        defaultLocale: 'de-DE'
-    }
+    output: 'export'
+    // i18n: {
+    //     // providing the locales supported by your application
+    //     // locales: ['en-US', 'de-DE'],
+    //     locales: ['de-DE'],
+    //     //  default locale used when the non-locale paths are visited
+    //     defaultLocale: 'de-DE'
+    // }
 }
+// const NextConfig = {
+//     output: 'export'
+// }
