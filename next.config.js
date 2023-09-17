10462a55b95aa54b1f7c5998607b58d475e9f942
Daniel Mikolai <d.mikolai@gmx.de>
2023-09-17 10:55:08 +0200

    out

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+    output: 'export',
     reactStrictMode: true,
     experimental: {
         appDir: true,
-        output: 'export',
-    },
-    i18n: {
-        // providing the locales supported by your application
-        // locales: ['en-US', 'de-DE'],
-        locales: ['de-DE'],
-        //  default locale used when the non-locale paths are visited
-        defaultLocale: 'de-DE',
+        output: 'export'
     }
+    // i18n: {
+    //     // providing the locales supported by your application
+    //     // locales: ['en-US', 'de-DE'],
+    //     locales: ['de-DE'],
+    //     //  default locale used when the non-locale paths are visited
+    //     defaultLocale: 'de-DE'
+    // }
 }
