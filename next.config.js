ba7e4a325eae32e0c42ca20ecdaff342b15345cf
Daniel Mikolai <d.mikolai@gmx.de>
2023-10-09 22:36:58 +0200

    env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    output: 'export',
+    // output: 'export',
     reactStrictMode: true,
     env: {
-        SECRET_CRYPTO_KEY: process.env.SECRET_CRYPTO_KEY
+        SECRET_CRYPTO_KEY: process.env.SECRET_CRYPTO_KEY,
+        TOKEN_BLACK: process.env.TOKEN_BLACK,
+        TOKEN_GREEN_JKD: process.env.TOKEN_GREEN_JKD,
+        TOKEN_BLUE_JKD: process.env.TOKEN_BLUE_JKD,
+        TOKEN_YELLOW_JKD: process.env.TOKEN_YELLOW_JKD,
+        TOKEN_GREEN_SHAOLIN: process.env.TOKEN_GREEN_SHAOLIN,
+        TOKEN_BLUE_SHAOLIN: process.env.TOKEN_BLUE_SHAOLIN,
+        TOKEN_YELLOW_SHAOLIN: process.env.TOKEN_YELLOW_SHAOLIN
     }
     // i18n: {
     //     // providing the locales supported by your application
